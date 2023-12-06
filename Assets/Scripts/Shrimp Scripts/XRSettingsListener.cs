141b35d8910a6afef0ee9a36e579757c19338714
Ajitesh-jam <ajitesh.jam@gmail.com>
2023-12-06 20:02:48 +0530

    Revert "lakh ka mahel 2"
    This reverts commit b3038a35b33b93ac13f8923e36f437491b82f515.

--- Assets/Scripts/Shrimp Scripts/XRSettingsListener.cs
+++ Assets/Scripts/Shrimp Scripts/XRSettingsListener.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
