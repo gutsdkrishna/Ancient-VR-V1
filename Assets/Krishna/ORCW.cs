f4755188363d414b18a589840b662c215b165caa
Krishna Paikine <119587231+gutsdkrishna@users.noreply.github.com>
2024-01-17 19:28:48 +0530

    DiE

--- Assets/Krishna/ORCW.cs
+++ Assets/Krishna/ORCW.cs
@@ -6,18 +6,30 @@ public class ORCW : MonoBehaviour
 {
     public int HP = 100;
     public Animator animator;
+    private bool isDead = false;
 
     public void TakeDamage(int damageAmount)
     {
+        if (isDead || animator.GetCurrentAnimatorStateInfo(0).IsName("Die"))
+        {
+            return; // If the enemy is already dead or dying, do nothing
+        }
+
         HP -= damageAmount;
         if (HP <= 0)
         {
-            animator.SetTrigger("die");
-            GetComponent<Collider>().enabled = false;
+            Die();
         }
         else
         {
             animator.SetTrigger("damage");
         }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+        animator.SetTrigger("die");
+        GetComponent<Collider>().enabled = false;
     }
 }
